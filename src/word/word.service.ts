c223e1877a29ee67a02863a31fcb5c445205afa2
Minh Nhat Truong <minhnhat091101@gmail.com>
2024-07-25 01:07:04 +0300

    Add error handling for word creation service

--- src/word/word.service.ts
+++ src/word/word.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 
@@ -7,20 +7,28 @@ export class WordService {
   constructor(private readonly databaseService: DatabaseService) { }
 
   create(topicId: string, createWordDto: Prisma.WordCreateInput) {
+    const normalizedWord = createWordDto.word.toString().toUpperCase().trim()
     return this.databaseService.word.create({
       data: {
-        word: createWordDto.word.toUpperCase(),
+        word: normalizedWord,
         topic: {
           connect: {
             id: topicId
           }
         }
       }
+    }).catch((error) => {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') { //Unique constraint
+          throw new BadRequestException(`Word '${normalizedWord}' has been added before!`)
+        }
+      }
+      throw error
     });
   }
 
   findAll() {
-    return this.databaseService.word.findMany();
+    return this.databaseService.word.findMany()
   }
 
   findOne(id: string) {
@@ -34,7 +42,7 @@ export class WordService {
   update(id: string, updateWordDto: Prisma.WordUpdateInput, topicId: string) {
     const dataToUpdate: any = {}
     if (updateWordDto.word) {
-      dataToUpdate.word = updateWordDto.word.toString().toUpperCase()
+      dataToUpdate.word = updateWordDto.word.toString().toUpperCase().trim()
     }
     if (topicId) {
       dataToUpdate.topic = {
